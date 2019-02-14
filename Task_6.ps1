c820a8572f6303d4794d1fadaac5afcaeda0d6b1
vdanilau <47238767+vdanilau@users.noreply.github.com>
2019-02-15 00:07:07 +0300

    Create Task_6.ps1

---
+++ Task_6.ps1
@@ -0,0 +1,9 @@
+﻿#6.	Вывести свободное место на локальных дисках. На каждом и сумму.
+$disk=Get-WmiObject -Class win32_logicaldisk -Filter "drivetype=3" |`
+ Select-Object DeviceID, Freespace
+$disk
+$s=$null
+foreach ($i in $disk.freespace){
+    $s+=$i/1Gb 
+}
+write-output "total free spase = $s Gb"
