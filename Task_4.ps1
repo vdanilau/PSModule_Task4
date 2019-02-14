0e9ae09750ac41d6170413a4a9281f60afc00c44
vdanilau <47238767+vdanilau@users.noreply.github.com>
2019-02-15 00:06:57 +0300

    Create Task_4.ps1

---
+++ Task_4.ps1
@@ -0,0 +1,3 @@
+﻿#4.	Вывести информацию об операционной системе, не менее 10 полей.
+Get-WmiObject Win32_OperatingSystem | select status, version, bootdevice, `
+Currenttimezone, localdatetime, freephysicalmemory, installdate, csname,` producttype,    SystemDrive
