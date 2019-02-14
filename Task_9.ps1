e9fb111ccb873f2fa5a389826bf7b3ea2fd11e82
vdanilau <47238767+vdanilau@users.noreply.github.com>
2019-02-15 00:07:28 +0300

    Create Task_9.ps1

---
+++ Task_9.ps1
@@ -0,0 +1,11 @@
+﻿#9.	Выводить сообщение при каждом запуске приложения MS Word.
+register-wmiEvent -query "select * from __instancecreationevent within 5 
+where targetinstance isa 'Win32_Process' and targetinstance.name='WINWORD.exe'" `
+-sourceIdentifier "ProcessStarted" -Action { Write-Host "Process WINWORD.exe was run" } 
+
+# Get-EventSubscriber | Unregister-Event
+
+
+
+
+
