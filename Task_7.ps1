161086e3bd4abb53fa3a9f129682d8317cf6d5a4
vdanilau <47238767+vdanilau@users.noreply.github.com>
2019-02-15 00:07:11 +0300

    Create Task_7.ps1

---
+++ Task_7.ps1
@@ -0,0 +1,17 @@
+﻿#7.Написать сценарий, выводящий суммарное время пингования компьютера (например 10.0.0.1) в сети.
+[CmdletBinding()]
+Param
+	(
+[parameter(Mandatory=$true)]
+[string]$IP
+    )
+$Totaltime = $null
+Get-WmiObject Win32_PingStatus -Filter "Address='$IP'" |`
+ForEach-Object{
+    $Totaltime += $_.ResponseTime
+}
+if ($Totaltime -ne $null)
+{
+Write-Host "TotalTime = " $Totaltime 
+}
+else {Write-Output "IP doesn't exist in your network!"}
