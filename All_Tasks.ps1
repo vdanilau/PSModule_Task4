d573cc40200f4bd494a2fdf37dfdec6c22d03d53
vdanilau <47238767+vdanilau@users.noreply.github.com>
2019-02-15 09:54:17 +0300

    Update All_Tasks.ps1

--- All_Tasks.ps1
+++ All_Tasks.ps1
@@ -54,11 +54,13 @@ Get-WmiObject win32_product | Select-Object name, version
 
 
 #9.	Выводить сообщение при каждом запуске приложения MS Word.
-register-wmiEvent -query "select * from __instancecreationevent within 5 
-where targetinstance isa 'Win32_Process' and targetinstance.name='WINWORD.exe'" `
+#9.	Выводить сообщение при каждом запуске приложения MS Word.
+register-wmiEvent -query "select * from __instancecreationevent within 5 where targetinstance isa 'Win32_Process' and targetinstance.name='WINWORD.exe'" `
 -sourceIdentifier "ProcessStarted" -Action { Write-Host "Process WINWORD.exe was run" } 
 
 # Get-EventSubscriber | Unregister-Event
+
+
 
 
 
